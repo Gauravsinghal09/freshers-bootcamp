084397e80c071db1a4dffeb6ad0d6c2f76d84e11
Gaurav Singhal <gsinghal@me.iitr.ac.in>
2021-07-25 18:31:25 +0530

    adds main file

---
+++ Day-3/problem-2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"Freshers_2021/Day-3/problem-2/Config"
+	"Freshers_2021/Day-3/problem-2/Models"
+	"Freshers_2021/Day-3/problem-2/Routes"
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
+
+func main() {
+	var er error
+	Config.DB, er = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	if er != nil {
+		fmt.Println("Status", er)
+	}
+	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.Student{})
+	routes := Routes.SetupRoutes()
+	routes.Run()
+}
